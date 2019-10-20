d3ccfcc3fce44838ba4f341510c4bedab753cccb
Jacopo Bucchioni <jacopo.bucchioni@stud.unifi.it>
2019-10-20 15:47:32 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
-#include "loginwindow.h"
+#include "mainwindow.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    LogInWindow w;
-    w.show();
+    MainWindow w;
+    //w.show();
     return a.exec();
 
 }
