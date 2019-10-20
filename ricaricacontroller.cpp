5d6de41f6ca08722886384e5ca5528ea546dd5b3
Jacopo Bucchioni <jacopo.bucchioni@stud.unifi.it>
2019-10-20 16:16:20 +0200

    Update ricaricacontroller.cpp

--- ricaricacontroller.cpp
+++ ricaricacontroller.cpp
@@ -22,27 +22,30 @@ RicaricaController::~RicaricaController()
 void RicaricaController::on_pushButton_ok_clicked()
 {
     double v = (ui->lineEdit_ricarica->text()).toDouble();
-    if(v>0){
-        QString s_pin = ui->lineEdit_pin->text();
-        if(s_pin.length()==4){
-            unsigned short int pin =s_pin.toUShort();
-            if(pinManager->isCorret(pin)){
+    QString s_pin = ui->lineEdit_pin->text();
+    if(s_pin.length()==4){
+        unsigned short int pin =s_pin.toUShort();
+        if(pinManager->isCorret(pin)){
+            if(v>0){
                 double s = saldo->getSaldo();
                 saldo->setSaldo((v+s));
                 transazioni->addTransizione("Ricarica di "+QString::number(v)+" euro -> saldo: "+QString::number(v+s));
                 Notify();
                 hide();
+            }else{
+                ui->label_errori->setStyleSheet("color: red");
+                ui->label_errori->setText("L'Importo deve essere >0");
             }
-        }
-        else {
+        }else {
             ui->label_errori->setStyleSheet("color: red");
             ui->label_errori->setText("PIN errato");
         }
-    }else{
+    }else {
         ui->label_errori->setStyleSheet("color: red");
-        ui->label_errori->setText("Importo errato");
+        ui->label_errori->setText("PIN errato");
     }
 }
+
 
 void RicaricaController::on_pushButton_cancel_clicked()
 {
