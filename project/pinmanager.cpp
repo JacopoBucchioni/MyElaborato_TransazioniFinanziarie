f282891d6755425803f6695b3667f0c37d2cd96b
Jacopo Bucchioni <jacopo.bucchioni@stud.unifi.it>
2020-04-06 15:03:10 +0200

    Update pinmanager.cpp

--- project/pinmanager.cpp
+++ project/pinmanager.cpp
@@ -7,15 +7,15 @@ PinManager::PinManager()
 {
   pintxt = new QFile("pin.txt");
   SimpleCrypt crypto(Q_UINT64_C(0x0be919fe8c95bc14)); //some random number
-  if(!((*pintxt).exists("pin.txt"))){
+  if(!(pintxt->exists("pin.txt"))){
 
-      (*pintxt).open(QIODevice::ReadWrite | QFile::Text);
+      pintxt->open(QIODevice::ReadWrite | QFile::Text);
       QTextStream stream(pintxt);
       unsigned short int p = 0000; //DEFAULT PIN
       QString pin_crypted = crypto.encryptToString(QString::number(p));
       stream<<pin_crypted;
-      (*pintxt).flush();
-      (*pintxt).close();
+      pintxt->flush();
+      pintxt->close();
     }
 }
 
@@ -40,28 +40,28 @@ bool PinManager::changePin(const unsigned short pin, const unsigned short newPin
 
 
 bool PinManager::isEqual(const unsigned short p) const{
-  (*pintxt).open(QIODevice::ReadOnly | QFile::Text);
+  pintxt->open(QIODevice::ReadOnly | QFile::Text);
   QTextStream stream(pintxt);
   SimpleCrypt crypto(Q_UINT64_C(0x0be919fe8c95bc14)); //some random number
   QString pin_decrypted = crypto.decryptToString(stream.readAll());
   if(p == pin_decrypted.toUShort()){
-      (*pintxt).flush();
-      (*pintxt).close();
+      pintxt->flush();
+      pintxt->close();
       return true;
     }
   else{
-      (*pintxt).flush();
-      (*pintxt).close();
+      pintxt->flush();
+      pintxt->close();
       return false;
     }
 }
 
 void PinManager::setPin(const unsigned short newPin) const{
-  (*pintxt).open(QIODevice::ReadWrite | QFile::Text);
+  pintxt->open(QIODevice::ReadWrite | QFile::Text);
   QTextStream stream(pintxt);
   SimpleCrypt crypto(Q_UINT64_C(0x0be919fe8c95bc14)); //some random number
   QString pin_crypted = crypto.encryptToString(QString::number(newPin));
   stream<<pin_crypted;
-  (*pintxt).flush();
-  (*pintxt).close();
+  pintxt->flush();
+  pintxt->close();
 }
